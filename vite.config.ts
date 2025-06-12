f543c62bf5dc39ab182a67a477bf4ac143417c10
matiaszenteno <mlzenteno@uc.cl>
2025-06-12 00:54:29 -0400

    general fixes

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
  
 export default defineConfig({
   plugins: [react()],
-  base: '/benefits-frontend/'
+  base: '/benefits-frontend/',
+  build: {
+    outDir: 'docs'
+  }
 }) 
